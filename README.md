ced5d1af1e680b18e78b21bd03e958f1b51a8579
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-29 21:42:54 -0500

    Removing useless comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # zig-sdl-game
+
+
+## References 
+- https://zigistry.dev/packages/castholm/SDL/
+- https://github.com/castholm/SDL
--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,7 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("Gary Run `zig build test` to run the tests.\n", .{});
 
     try bw.flush(); // Don't forget to flush!
 }
