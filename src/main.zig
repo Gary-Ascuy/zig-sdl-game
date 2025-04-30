9463cb92dc4a2f1fc88a2d89007e9d6c22209b25
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-29 23:07:19 -0500

    Refactoring main

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,6 @@ const sdl = @import("sdl.zig");
 pub fn main() !u8 {
     sdl.app_err.reset();
     var empty_argv: [0:null]?[*:0]u8 = .{};
-    const status: u8 = @truncate(@as(c_uint, @bitCast(c.SDL_RunApp(empty_argv.len, @ptrCast(&empty_argv), sdl.sdlMainC, null))));
-    return sdl.app_err.load() orelse status;
+    const status = c.SDL_RunApp(empty_argv.len, @ptrCast(&empty_argv), sdl.sdlMainC, null);
+    return sdl.app_err.load() orelse @truncate(@as(c_uint, @bitCast(status)));
 }
